14ba691e0e58d9f2b8e533993f3b129fcc259137
furytm <kanuemma367@gmail.com>
2025-10-13 21:22:32 +0100

    revrted two rows mobile

--- src/views/menu-page.tsx
+++ src/views/menu-page.tsx
@@ -1725,7 +1725,7 @@ export default function MenuPage() {
           </div>
 
           {/* Menu Items Grid */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {filteredItems.map((item, index) => (
               <div
                 key={item.id}
